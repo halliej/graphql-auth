9bcd3e5790a95c696dc26d7fb45eb67dcfb7d1df
Hal DeLage <hal.delage@teamaol.com>
2017-03-29 13:47:43 -0400

    Added redirect to content on successful signup

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 import query from '../queries/CurrentUser';
@@ -8,6 +9,12 @@ class SignupForm extends Component {
   constructor(props) {
     super(props);
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
@@ -33,4 +40,6 @@ class SignupForm extends Component {
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
